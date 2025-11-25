8b7723bdae308e0e8519d0043eabc9675bf29129
Cristian <cristiangcabral@gmail.com>
2025-11-25 10:40:23 -0300

    FIX archivos estáticos para deploy

--- tienda_artaud/settings.py
+++ tienda_artaud/settings.py
@@ -218,7 +218,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Define dónde Django pondrá los archivos al hacer deploy
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if not DEBUG:
     # Configuración para producción
