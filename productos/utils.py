0f7d9dd8007dc099b43d0ed69a3b5dcd944ecb42
Cristian <cristiangcabral@gmail.com>
2025-11-25 10:48:13 -0300

    zxing-cpp en lugar de pyzbar, para deploy

--- productos/utils.py
+++ productos/utils.py
@@ -1,18 +1,21 @@
-from pyzbar.pyzbar import decode
 from PIL import Image
+import zxingcpp
 
 def decodificar_imagen(image_file):
     """
-    Recibe un archivo de imagen (en memoria), lo abre con Pillow y trata de leer códigos de barras o QR.
-    Retorna el string del código o None.
+    Recibe un archivo de imagen, lo abre con Pillow
+    y lee códigos de barras usando zxing-cpp.
     """
     try:
         img = Image.open(image_file)
-        codigos = decode(img) # Devuelve una lista de códigos encontrados
         
-        if codigos:
-            # Devuelve el primer código encontrado
-            return codigos[0].data.decode('utf-8') # .data viene en bytes, hay que decodificar a utf-8
+        # zxingcpp puede leer directamente objetos de Pillow
+        resultados = zxingcpp.read_barcodes(img)
+        
+        if resultados:
+            # Devuelve el texto del primer código encontrado
+            return resultados[0].text
+            
     except Exception as e:
         print(f"Error al leer código de barras: {e}")
     
